5a93afe515330775ffd724212a2ee2bd2ff2c98e
Luiz Fernando <lfaires@gmail.com>
2021-05-17 23:35:19 -0300

    styled-component global

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import GlobalStyle from './globalStyles'
 import LoginPage from './LoginPage'
 
 export default function App() {
     return(
        <BrowserRouter>
+            <GlobalStyle/>
             <Switch>
                 <Route>
                     <LoginPage path="/" exact/>
