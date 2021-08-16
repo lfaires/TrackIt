0a1b6184a411de613d14fb2a6e1b262c8e6bc319
Luiz Fernando <lfaires@gmail.com>
2021-08-16 19:33:31 -0300

    refactor: signUp page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -7,73 +7,105 @@ import Loader from 'react-loader-spinner'
 import logotipo from '../assets/images/logotipo.png';
 
 export default function SignUp({validURL, validEmail}) {
-    const [name, setName] = useState("")
-    const [image, setImage] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [isDisabled, setIsDisabled] = useState(false);
-    const history = useHistory()
-    
-    function validation() {
-        if (!validEmail(email) || email === ""){alert("Insira um email válido");return }
-        if (password === ""){alert("insira uma senha");return}
-        if (name === ""){alert("insira o nome do perfil");return}
-        if (!validURL(image) || image === ""){alert("Insira uma URL válida");return}
-        return true
-    }
+  const [name, setName] = useState("")
+  const [image, setImage] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isDisabled, setIsDisabled] = useState(false);
+  const history = useHistory()
 
-    function signUp(){
-        if (!validation()){return}
-        setIsDisabled(true)
-        const body ={ email, name, image, password}
+  function validation() {
+    if (!validEmail(email) || email === ""){alert("Insira um email válido");return }
+    if (password === ""){alert("insira uma senha");return}
+    if (name === ""){alert("insira o nome do perfil");return}
+    if (!validURL(image) || image === ""){alert("Insira uma URL válida");return}
+    return true
+  }
 
-        const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up",body)
+  function submit(e) {
+    e.preventDefault();   
+    if (!validation()){return}
+    setIsDisabled(true)
+    const body ={ email, name, image, password}
+    const request = axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/sign-up`,body)
 
-        request.then( response => {
-            history.push("/")
-            alert("Cadastro feito com sucesso!")
-        })
-        request.catch( (error) => {
-            alert("Tente novamente!")
-            setIsDisabled(false)
-            history.push("/cadastro")
-            console.log(error)
-        })
-    }
+    request.then(() => {
+        history.push("/")
+        alert("Cadastro feito com sucesso!")
+    })
 
-    return (
-        <Container>
-            <Logo src={logotipo}></Logo>
-            <Title>TrackIt</Title>
-            <Input type="text" placeholder="email" value={email} onChange={ e => setEmail(e.target.value)} disabled={isDisabled}></Input>
-            <Input type="password" placeholder="senha" value={password} onChange={ e => setPassword(e.target.value)} disabled={isDisabled}></Input>
-            <Input type="text" placeholder="nome" value={name} onChange={ e => setName(e.target.value)} disabled={isDisabled}></Input>
-            <Input type="text" placeholder="foto" value={image} onChange={ e => setImage(e.target.value)} disabled={isDisabled}></Input>
-            <Button onClick={signUp} disabled={isDisabled}>{ !isDisabled ? "Cadastrar" : <Loader type="ThreeDots" color="#FFF" height={15}/>}</Button>
-            <StyledLink to="/">Já tem uma conta? Faça login!</StyledLink>
-        </Container>
+    request.catch(() => {
+        alert("Tente novamente!")
+        setIsDisabled(false)
+        history.push("/cadastro")
+    })
+  }
+
+  return (
+    <Page>
+      <Form onSubmit={submit}>
+        <Logo src={logotipo}></Logo>
+        <Title>TrackIt</Title>
+        <Input 
+            type="email" 
+            placeholder="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            disabled={isDisabled}
+        />
+        <Input 
+            type="password" 
+            placeholder="senha" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            disabled={isDisabled}
+        />
+        <Input 
+            type="text" 
+            placeholder="nome" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)} 
+            disabled={isDisabled}
+        />
+        <Input 
+            type="text" 
+            placeholder="foto" 
+            value={image} 
+            onChange={(e) => setImage(e.target.value)} 
+            disabled={isDisabled}
+        />
+        <Button disabled={isDisabled}>
+          { !isDisabled ? "Cadastrar" : <Loader type="ThreeDots" color="#FFF" height={15} />}
+        </Button>
+        <StyledLink to="/">Já tem uma conta? Faça login!</StyledLink>
+      </Form>
+    </Page>
     )
 }
 
-const Container = styled.div`
+const Page = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     background: #FFF;
     padding: 68px 36px 97px 36px;
+`;
+const Form = styled.form`
+    text-align: center;
+    padding: 3%;
 `
 const Logo = styled.img`
     width: 42vw;
     height: 86.23px;
-`
+`;
 const Title = styled.div`
     font: 69px Playball;
     color: #126BA5;
     margin-bottom: 32px;
-`
+`;
 const Input = styled.input`
-    width: 81vw;
+    width: 100%;
     height: 45px;
     margin-bottom: 6px; 
     border: 1px solid #D4D4D4;
@@ -82,14 +114,14 @@ const Input = styled.input`
 
     &::placeholder {
         font-size: 20px;
-        color: #DBDBDB;
+        color: #c5c5c5;
     }
-`
+`;
 const Button = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 81vw;
+    width: 100%;
     height: 45px;
     color: #FFF;
     font-size: 21px;
@@ -98,9 +130,9 @@ const Button = styled.button`
     border-radius: 5px;
     border: none;
     margin-bottom: 25px;
-`
+`;
 const StyledLink = styled(Link)`
     font-size: 14px;
     color: #52B6FF;
     text-decoration: underline; 
-`
+`;
